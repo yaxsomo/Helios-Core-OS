09617c187d4e59472ab5e26b55a60384a81cb34b
yaxsomo <yassine.dehhani@ynov.com>
2024-06-01 14:29:47 +0200

    Updated installation procedure script

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+sudo locale-gen en_US.UTF-8
+
 # Update package list
 sudo apt-get update
+sudo apt-get upgrade -y
 
 # Install packages required for builds
 sudo apt-get -f -y install \
@@ -12,3 +15,24 @@ sudo apt-get -f -y install \
     cpio file zstd lz4
 # Install host packages
 sudo apt-get install xinetd tftpd nfs-kernel-server minicom build-essential libncurses5-dev autoconf automake dos2unix screen lrzsz lzop u-boot-tools
+
+
+
+git clone https://git.yoctoproject.org/poky
+cd poky/
+git checkout -t origin/kirkstone -b helios-kirkstone
+
+
+git clone https://git.yoctoproject.org/meta-arm
+cd meta-arm/
+git checkout -t origin/kirkstone -b helios-kirkstone
+cd ..
+
+
+git clone https://github.com/openembedded/meta-openembedded.git
+cd meta-openembedded/
+git checkout -t origin/kirkstone -b helios-kirkstone
+cd ..
+
+
+git clone https://github.com/yaxsomo/meta-helios-splashscreen.git
